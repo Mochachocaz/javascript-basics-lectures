31b0d5b0edebaad211380830199508d654c407d3
Sam Martin <sammartin1357@gmail.com>
2024-03-18 21:59:13 +0800

    initial commit to github

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log('Hello World!');
+var firstname = "Sam";
+let lastname = 'Martin';
+
+firstname = 'Will';
+lastname = 'Johnson';
+
+const fullname = 'Sam Martin';
+
+fullname = 'Will Johnson';
+
+console.log(firstname);
+console.log(lasname);
+console.log(fullname);
