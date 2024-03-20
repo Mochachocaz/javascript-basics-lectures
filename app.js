7c9de2c8313423bf942b8630798e8e6f43e6f487
Sam Martin <sammartin1357@gmail.com>
2024-03-20 22:24:05 +0800

    added more variables to js

--- app.js
+++ app.js
@@ -12,3 +12,16 @@ fullname = 'Will Johnson';
 console.log(firstname);
 console.log(lastname);
 console.log(fullname);
+
+
+
+var firstname = "Sam";
+var age = 25; 
+var isMale = true; 
+var isFemale = false;
+var surname = null;
+var person = {
+    name: firstname, 
+    age: age
+};
+person.age; 
